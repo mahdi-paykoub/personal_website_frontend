f523f1dfe8e7e6b44f95400e6b4d838e411dfa1b
mahdipaykoub <mahdi.paykoub.github@gmail.com>
2025-03-23 23:19:52 +0330

    add example works

--- js/js.js
+++ js/js.js
@@ -5,7 +5,6 @@
 //     body.classList.toggle('dark-mode');
 // });
 
-
 // music player
 const audio = document.getElementById('audio');
 const playPauseBtn = document.getElementById('playPauseBtn');
@@ -19,74 +18,70 @@ function formatTime(seconds) {
     return `${minutes}:${secs < 10 ? '0' : ''}${secs}`;
 }
 
-audio.addEventListener('timeupdate', () => {
-    currentTimeDisplay.textContent = formatTime(audio.currentTime);
-    const remainingTime = audio.duration - audio.currentTime;
-    durationDisplay.textContent = formatTime(remainingTime);
-    const progress = (audio.currentTime / audio.duration) * 100;
-    progressBar.style.width = `${progress}%`;
-});
 
-audio.addEventListener('loadedmetadata', () => {
-    durationDisplay.textContent = formatTime(audio.duration);
-});
-audio.addEventListener('ended', () => {
-    playIcon.style.display = 'block';
-    pauseIcon.style.display = 'none';
-    progressBar.style.width = '0%';
-    currentTimeDisplay.textContent = '0:00'; // بازگرداندن زمان پخش شده به حالت اولیه
-});
+if (audio && progressBar && currentTimeDisplay && durationDisplay) {
+    audio.addEventListener('timeupdate', () => {
+        currentTimeDisplay.textContent = formatTime(audio.currentTime);
+        const remainingTime = audio.duration - audio.currentTime;
+        durationDisplay.textContent = formatTime(remainingTime);
+        const progress = (audio.currentTime / audio.duration) * 100;
+        progressBar.style.width = `${progress}%`;
+    });
+    audio.addEventListener('ended', () => {
+        playIcon.style.display = 'block';
+        pauseIcon.style.display = 'none';
+        progressBar.style.width = '0%';
+        currentTimeDisplay.textContent = '0:00'; // بازگرداندن زمان پخش شده به حالت اولیه
+    });
+    audio.addEventListener('loadedmetadata', () => {
+        durationDisplay.textContent = formatTime(audio.duration);
+    });
+    audio.addEventListener('timeupdate', () => {
+        const progress = (audio.currentTime / audio.duration) * 100;
+        progressBar.style.width = `${progress}%`;
+    });
+    progressBar.parentElement.addEventListener('click', (e) => {
+        const clickX = e.offsetX;
+        const width = progressBar.parentElement.clientWidth;
+        const duration = audio.duration;
+        audio.currentTime = (clickX / width) * duration;
+    });
+}
 
-playPauseBtn.addEventListener('click', () => {
-    if (audio.paused) {
-        audio.play();
-        playIcon.style.display = 'none';
-        pauseIcon.style.display = 'block';
-    } else {
+if (playPauseBtn) {
+    playPauseBtn.addEventListener('click', () => {
+        if (audio.paused) {
+            audio.play();
+            playIcon.style.display = 'none';
+            pauseIcon.style.display = 'block';
+        } else {
+            audio.pause();
+            pauseIcon.style.display = 'none';
+            playIcon.style.display = 'block';
+        }
+    });
+}
+
+const myModal = document.getElementById('musicPlayerModel');
+if (myModal) {
+    myModal.addEventListener('hidden.bs.modal', () => {
+        const audio = document.getElementById('audio');
         audio.pause();
         pauseIcon.style.display = 'none';
         playIcon.style.display = 'block';
-    }
-});
-
-audio.addEventListener('timeupdate', () => {
-    const progress = (audio.currentTime / audio.duration) * 100;
-    progressBar.style.width = `${progress}%`;
-});
-
-progressBar.parentElement.addEventListener('click', (e) => {
-    const clickX = e.offsetX;
-    const width = progressBar.parentElement.clientWidth;
-    const duration = audio.duration;
-    audio.currentTime = (clickX / width) * duration;
-});
-
-const myModal = document.getElementById('musicPlayerModel');
-myModal.addEventListener('hidden.bs.modal', () => {
-    const audio = document.getElementById('audio');
-    audio.pause();
-    pauseIcon.style.display = 'none';
-    playIcon.style.display = 'block';
-    audio.currentTime = 0;
-});
+        audio.currentTime = 0;
+    });
+}
 
 
-
-
-// swiper
 const swiper = new Swiper('.swiper', {
-    // Default parameters
     slidesPerView: 1,
     spaceBetween: 10,
-    // Responsive breakpoints
     breakpoints: {
-        // when window width is >= 320px
         320: {
             slidesPerView: 1.8,
             spaceBetween: 20
         },
-
-        // when window width is >= 640px
         992: {
             slidesPerView: 5,
             spaceBetween: 15
@@ -94,7 +89,6 @@ const swiper = new Swiper('.swiper', {
     }
 })
 
-// swiper
 const swiper_tech = new Swiper('.swiper-tech', {
     slidesPerView: 1,
     spaceBetween: 10,
@@ -119,7 +113,6 @@ const swiper_tech = new Swiper('.swiper-tech', {
 })
 
 
-// swiper
 const swiper_register_road = new Swiper('.swiper_register_road', {
     slidesPerView: 1,
     spaceBetween: 10,
@@ -143,7 +136,6 @@ const swiper_register_road = new Swiper('.swiper_register_road', {
     }
 })
 
-// swiper
 const laravel_service_swiper = new Swiper('.laravel-service-swiper', {
     slidesPerView: 1,
     spaceBetween: 10,
@@ -169,20 +161,41 @@ const laravel_service_swiper = new Swiper('.laravel-service-swiper', {
     },
 })
 
-const swiperCard = new Swiper('.swiperCard', {
-    effect: "cards",
-    initialSlide: 2
-});
+
 
 document.addEventListener("DOMContentLoaded", function () {
     const textElement = document.getElementById("text");
     const button = document.getElementById("toggleBtn");
     let isExpanded = false;
 
-    button.addEventListener("click", () => {
-        isExpanded = !isExpanded;
-        textElement.classList.toggle("expanded", isExpanded);
-        textElement.classList.toggle("fade-text", !isExpanded);
-        button.textContent = isExpanded ? "نمایش کمتر" : "نمایش بیشتر";
-    });
+    if (button) {
+        button.addEventListener("click", () => {
+            isExpanded = !isExpanded;
+            textElement.classList.toggle("expanded", isExpanded);
+            textElement.classList.toggle("fade-text", !isExpanded);
+            button.textContent = isExpanded ? "نمایش کمتر" : "نمایش بیشتر";
+        });
+    }
+
 });
+
+const cardSwiper = new Swiper('.cardSwiper', {
+    effect: "cards",
+    grabCursor: true,
+    initialSlide: 2,
+});
+
+const swiperswiper_shop_ex = new Swiper('.swiper_shop_ex', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints: {
+        320: {
+            slidesPerView: 1.8,
+            spaceBetween: 20
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 15
+        }
+    }
+})
