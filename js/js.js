107a6b90605481f10dba437daa4c5ffe08e3ccac
mahdi paykoub <mahdi.paykoub.github@gmail.com>
2025-06-02 08:30:36 -0800

    coding call action box

--- js/js.js
+++ js/js.js
@@ -1,79 +1,3 @@
-// const themeToggle = document.getElementById('theme-toggle');
-// const body = document.body;
-
-// themeToggle.addEventListener('click', () => {
-//     body.classList.toggle('dark-mode');
-// });
-
-// music player
-const audio = document.getElementById('audio');
-const playPauseBtn = document.getElementById('playPauseBtn');
-const progressBar = document.getElementById('progressBar');
-const currentTimeDisplay = document.getElementById('currentTime');
-const durationDisplay = document.getElementById('duration');
-
-function formatTime(seconds) {
-    const minutes = Math.floor(seconds / 60);
-    const secs = Math.floor(seconds % 60);
-    return `${minutes}:${secs < 10 ? '0' : ''}${secs}`;
-}
-
-
-if (audio && progressBar && currentTimeDisplay && durationDisplay) {
-    audio.addEventListener('timeupdate', () => {
-        currentTimeDisplay.textContent = formatTime(audio.currentTime);
-        const remainingTime = audio.duration - audio.currentTime;
-        durationDisplay.textContent = formatTime(remainingTime);
-        const progress = (audio.currentTime / audio.duration) * 100;
-        progressBar.style.width = `${progress}%`;
-    });
-    audio.addEventListener('ended', () => {
-        playIcon.style.display = 'block';
-        pauseIcon.style.display = 'none';
-        progressBar.style.width = '0%';
-        currentTimeDisplay.textContent = '0:00'; // بازگرداندن زمان پخش شده به حالت اولیه
-    });
-    audio.addEventListener('loadedmetadata', () => {
-        durationDisplay.textContent = formatTime(audio.duration);
-    });
-    audio.addEventListener('timeupdate', () => {
-        const progress = (audio.currentTime / audio.duration) * 100;
-        progressBar.style.width = `${progress}%`;
-    });
-    progressBar.parentElement.addEventListener('click', (e) => {
-        const clickX = e.offsetX;
-        const width = progressBar.parentElement.clientWidth;
-        const duration = audio.duration;
-        audio.currentTime = (clickX / width) * duration;
-    });
-}
-
-if (playPauseBtn) {
-    playPauseBtn.addEventListener('click', () => {
-        if (audio.paused) {
-            audio.play();
-            playIcon.style.display = 'none';
-            pauseIcon.style.display = 'block';
-        } else {
-            audio.pause();
-            pauseIcon.style.display = 'none';
-            playIcon.style.display = 'block';
-        }
-    });
-}
-
-const myModal = document.getElementById('musicPlayerModel');
-if (myModal) {
-    myModal.addEventListener('hidden.bs.modal', () => {
-        const audio = document.getElementById('audio');
-        audio.pause();
-        pauseIcon.style.display = 'none';
-        playIcon.style.display = 'block';
-        audio.currentTime = 0;
-    });
-}
-
-
 const swiper = new Swiper('.swiper', {
     slidesPerView: 1,
     spaceBetween: 10,
@@ -215,22 +139,6 @@ function setActiveSlideStyle(swiper) {
     const activeIndex = swiper.activeIndex;
     swiper.slides[activeIndex].classList.add('swiper-slide-active-center');
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-    const textElement = document.getElementById("text");
-    const button = document.getElementById("toggleBtn");
-    let isExpanded = false;
-
-    if (button) {
-        button.addEventListener("click", () => {
-            isExpanded = !isExpanded;
-            textElement.classList.toggle("expanded", isExpanded);
-            textElement.classList.toggle("fade-text", !isExpanded);
-            button.textContent = isExpanded ? "نمایش کمتر" : "نمایش بیشتر";
-        });
-    }
-
-});
 
 const cardSwiper = new Swiper('.cardSwiper', {
     effect: "cards",
